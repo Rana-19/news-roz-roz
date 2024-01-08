e23e4daae91614c83fcbc4307090e2e67e9c1c7f
Rana-19 <138851183+Rana-19@users.noreply.github.com>
2024-01-08 11:58:32 +0530

    Update NewsActions.jsx

--- src/componets/Context/News/NewsActions.jsx
+++ src/componets/Context/News/NewsActions.jsx
@@ -1,6 +1,6 @@
-export const fetchNews = async (topic) => {
+export const fetchNews = async (topic,date) => {
     try {
-        const response = await fetch(`https://newsapi.org/v2/everything?q=${topic}&from=2023-10-08&sortBy=publishedAt&apiKey=2c7444ef89764061a75b38e1e3ca3ec5`)
+        const response = await fetch(`https://newsapi.org/v2/everything?q=${topic}&from=${date}&sortBy=publishedAt&apiKey=2c7444ef89764061a75b38e1e3ca3ec5`)
         const data = await response.json()
         // console.log(data);
         return data.articles
